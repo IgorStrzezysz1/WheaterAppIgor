f31243407e68867fbe21a62b733fa1b12fe8a386
IgorStrzezysz1 <igor.strzezysz@wp.pl>
2024-12-11 18:03:42 +0100

    Upade

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config();//tego nie rozumiem?
 
-const app = express();
-const PORT = 3000;
+const app = express(); //tego nie rozumiem?
+const PORT = 3000;//tego nie rozumiem?
 
 app.use(express.static('public')); // Obsługa plików statycznych (np. HTML, CSS)
 
 // Endpoint do pobierania danych pogodowych
-app.get('/weather', async (req, res) => {
-    const city = req.query.city;
-    const apiKey = process.env.API_KEY;
+app.get('/weather', async (req, res) => {//tego nie rozumiem?
+    const city = req.query.city;//tego nie rozumiem?
+    const apiKey = process.env.API_KEY;//tego nie rozumiem?
 
     if (!city) {
-        return res.status(400).json({ error: 'Brak nazwy miasta w żądaniu.' });
+        return res.status(400).json({ error: 'Brak nazwy miasta w żądaniu.' });//tego nie rozumiem?
     }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pl`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pl`;//tego nie rozumiem?
 
     try {
         const response = await fetch(url);
